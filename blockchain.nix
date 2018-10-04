c652ca544abcfbc20f4dd962c0db9f97baa268d1
Daniel Schaefer <git@danielschaefer.me>
2018-10-05 00:30:28 +0200

    Calculate hash from previous entry

--- blockchain.nix
+++ blockchain.nix
@@ -1,10 +1,14 @@
-{ mkDerivation, base, cryptonite, stdenv, text }:
+{ mkDerivation, base, bytestring, cryptonite, memory, stdenv, text
+, time
+}:
 mkDerivation {
   pname = "blockchain";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base cryptonite text ];
+  executableHaskellDepends = [
+    base bytestring cryptonite memory text time
+  ];
   license = stdenv.lib.licenses.bsd3;
 }
--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,25 @@
 {-# LANGUAGE OverloadedStrings    #-}
 {-# LANGUAGE RecordWildCards      #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 
 module Main where
 
-import           Text.Printf      (printf)
-import           Data.Text        (Text, unpack)
-import           Data.ByteString  (ByteString)
-import           Data.Time.Clock  (getCurrentTime, UTCTime)
-import           Data.Time.Format (formatTime, defaultTimeLocale)
+import           Data.ByteArray     (convert, append, ByteArray)
+import           Data.ByteString    (ByteString)
+import           Data.Monoid        ((<>))
+import           Data.Text          (Text, pack, unpack)
+import           Data.Text.Encoding (encodeUtf8)
+import           Data.Time.Clock    (getCurrentTime, UTCTime)
+import           Data.Time.Format   (formatTime, defaultTimeLocale)
+import           Text.Printf        (printf)
 
-import           Crypto.Hash      (Digest, SHA256)
-import qualified Crypto.Hash      (hash)
+import           Crypto.Hash        (Digest, SHA256)
+import qualified Crypto.Hash        (hash)
 
-data BlockNumber = BlockNumber Integer -- uint64, min 2 chars
-instance Show BlockNumber where
-    show (BlockNumber num) =  printf "%02d" num
+type BlockNumber = Integer -- uint64, min 2 chars
+
+showBn :: BlockNumber -> String
+showBn num =  printf "%02d" num
 
 data Content =
     Content { index      :: BlockNumber
@@ -24,39 +29,53 @@ data Content =
             , text       :: Text -- UTF8, no ; or \n
             }
 
+instance Show Content where
+    show (Content i d g p t) =
+                  showBn i
+        ++ ";" ++ (formatTime defaultTimeLocale "%d.%m.%y-%H:%M:%S" d)
+        ++ ";" ++ showBn g
+        ++ ";" ++ showBn p
+        ++ ";" ++ unpack t
+
 data Entry =
     Entry { content :: Content
           , hash    :: Digest SHA256 -- SHA256 over string representation
           }
 
 instance Show Entry where
-    show (Entry (Content i d g p t) h) =
-                  show i
-        ++ ";" ++ (formatTime defaultTimeLocale "%d.%m.%y-%H:%M:%S" d)
-        ++ ";" ++ show g
-        ++ ";" ++ show p
-        ++ ";" ++ unpack t
-        ++ ";" ++ show h
+    show (Entry content h) = show content ++ ";" ++ show h
 
 type Chain = [Entry]
 
-contentHash (Content i d g p t) = Crypto.Hash.hash ("" :: ByteString)
+hashContent :: Content -> Digest SHA256
+hashContent content = Crypto.Hash.hash . encodeUtf8 . pack $ show content
 
-newEntry content Nothing = Entry content (contentHash content)
+newEntry :: Content -> Maybe Entry -> Entry
+newEntry content Nothing = Entry content (hashContent content)
+newEntry content (Just prev) =
+    Entry content entryHash
+  where
+      prevHash    :: ByteString = convert $ hashContent content
+      contentHash :: ByteString = convert $ hash prev
+      entryHash   = Crypto.Hash.hash $ prevHash <> (encodeUtf8 ";") <> contentHash
 
-newEntry content (Just prev) = Entry content entryHash
-    where entryHash = Crypto.Hash.hash ("" :: ByteString)
+mapWithPrev :: (Maybe b -> a -> b) -> [a] -> [b]
+mapWithPrev fun list = foo [] list
+    where foo [] (x:xs) = foo [fun Nothing x] xs
+          foo (d:ds) (x:xs) = foo ((fun (Just d) x):d:ds) xs
+          foo done [] = done
 
 main :: IO ()
 main = do
     now <- getCurrentTime
 
-    let chain = [newEntry (Content (BlockNumber 1)
-                                   now
-                                   (BlockNumber 1)
-                                   (BlockNumber 1)
-                                   "Foo")
-                          Nothing
-                ]
+    let contents = [ Content 1 now 1 1 "Foo"
+                   , Content 1 now 1 1 "Foo"
+                   , Content 1 now 1 1 "Foo"
+                   , Content 1 now 1 1 "Foo"
+                   ]
+        chain :: Chain = mapWithPrev
+                  (\prev new -> newEntry new prev)
+                  contents
 
     mapM_ (\entry -> appendFile "foo.log" $ show entry ++ "\n") chain
