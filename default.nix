80106f225c2afc70afd0cc3db94cd429a9093465
Daniel Schaefer <git@danielschaefer.me>
2018-10-16 13:24:38 +0200

    Call cabal2nix from nix

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 let
-  drv = haskellPackages.callPackage ./blockchain.nix {};
+  drv = haskellPackages.callCabal2nix "blockchain-johnazoidberg" (lib.cleanSource ./.) {};
 in
   if pkgs.lib.inNixShell then drv.env else drv
